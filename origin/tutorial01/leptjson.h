2d1a59e5af7c1db76735cb1bed93c9a5c73decf5
CHAZHOU <7907055+chazhou@user.noreply.gitee.com>
2024-01-11 16:38:35 +0800

    tutorial02

--- origin/tutorial01/leptjson.h
+++ origin/tutorial01/leptjson.h
@@ -1,21 +1,30 @@
 #ifndef LEPTJSON_H__
 #define LEPTJSON_H__
 
-typedef enum { LEPT_NULL, LEPT_FALSE, LEPT_TRUE, LEPT_NUMBER, LEPT_STRING, LEPT_ARRAY, LEPT_OBJECT } lept_type;
+/* JSON中包含的6中数据类型，加上false/true共七种 */
+typedef enum {
+  LEPT_NULL,
+  LEPT_FALSE,
+  LEPT_TRUE,
+  LEPT_NUMBER,
+  LEPT_STRING,
+  LEPT_ARRAY,
+  LEPT_OBJECT
+} lept_type;
 
 typedef struct {
-    lept_type type;
-}lept_value;
+  lept_type type;
+} lept_value;
 
 enum {
-    LEPT_PARSE_OK = 0,
-    LEPT_PARSE_EXPECT_VALUE,
-    LEPT_PARSE_INVALID_VALUE,
-    LEPT_PARSE_ROOT_NOT_SINGULAR
+  LEPT_PARSE_OK = 0,
+  LEPT_PARSE_EXPECT_VALUE,     /* 只含有空白 */
+  LEPT_PARSE_INVALID_VALUE,    /* 一个值在空白后还有其他的值 */
+  LEPT_PARSE_ROOT_NOT_SINGULAR /* 值不是三种字面值 */
 };
 
-int lept_parse(lept_value* v, const char* json);
+int lept_parse(lept_value *v, const char *json);
 
-lept_type lept_get_type(const lept_value* v);
+lept_type lept_get_type(const lept_value *v);
 
 #endif /* LEPTJSON_H__ */
